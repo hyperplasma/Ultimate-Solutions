c2cd86882219b5f0e8ef77b232a780789791ccac
akira37 <863781751@qq.com>
2024-12-30 21:52:59 +0800

    LC 3

--- src/main/java/LC2/AddTwoNumbers.java
+++ src/main/java/LC2/AddTwoNumbers.java
@@ -1,5 +1,34 @@
 package LC2;
 
-public class AddTwoNumbers {
+class Solution {
 
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode l3 = new ListNode();
+        ListNode p = l1;
+        ListNode q = l2;
+        ListNode r = l3;
+
+        int carry = 0;
+        while (p != null || q != null || carry > 0) {
+            int sum = carry;
+            if (p != null) {
+                sum += p.val;
+            }
+            if (q != null) {
+                sum += q.val;
+            }
+            carry = sum / 10;
+
+            r.next = new ListNode(sum % 10);
+            r = r.next;
+            if (p != null) {
+                p = p.next;
+            }
+            if (q != null) {
+                q = q.next;
+            }
+        }
+
+        return l3.next;
+    }
 }
