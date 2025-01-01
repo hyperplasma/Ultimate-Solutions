0112706583d08b4d1296e891c479d39b98e000eb
akira37 <863781751@qq.com>
2025-01-01 15:23:52 +0800

    Sort all files for future use

---
+++ LeetCode/java/LC0003_LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,61 @@
+package LC0003_LongestSubstringWithoutRepeatingCharacters;
+
+import java.util.*;
+
+/**
+ * <a href="https://leetcode.com/problems/longest-substring-without-repeating-characters/">Longest Substring Without Repeating Characters</a>
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLongestSubstring("pwwkew"));
+    }
+}
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() < 2) return s.length();
+
+        boolean[] map = new boolean[128];
+        Arrays.fill(map, false);
+        map[s.charAt(0)] = true;
+
+        int i = 0;
+        int maxLength = 1;
+        for (int j = 1; j < s.length(); j++) {
+
+            if (!map[s.charAt(j)]) {
+                maxLength = Math.max(maxLength, j - i + 1);
+            } else {
+                while (map[s.charAt(j)]) {
+                    map[s.charAt(i++)] = false;
+                }
+            }
+
+            map[s.charAt(j)] = true;
+        }
+
+        return maxLength;
+    }
+
+//    public int lengthOfLongestSubstring(String s) {
+//        StringBuilder sb = new StringBuilder();
+//        if (!s.isEmpty()) {
+//            sb.append(s.charAt(0));
+//        }
+//
+//        int maxLength = 0;
+//        for (int i = 0; i < s.length(); i++) {
+//            int index = sb.indexOf(String.valueOf(s.charAt(i)));
+//
+//            if (index != -1) {
+//                sb.delete(0, index + 1);
+//            }
+//            sb.append(s.charAt(i));
+//
+//            maxLength = Math.max(maxLength, sb.length());
+//        }
+//
+//        return maxLength;
+//    }
+}
---
+++ LeetCode/java/LC0009_PalindromeNumber/PalindromeNumber.java
@@ -0,0 +1,45 @@
+package LC0009_PalindromeNumber;
+
+/**
+ * <a href="https://leetcode.com/problems/palindrome-number/">Palindrome Number</a>
+ */
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isPalindrome(121));
+    }
+}
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        if (x % 10 == 0 && x != 0) {
+            return false;
+        }
+
+        int temp = 0;
+        while (x > temp) {
+            temp = temp * 10 + x % 10;
+            if (x == temp) break;
+            x /= 10;
+            if (x == temp) break;
+        }
+        return x == temp;
+    }
+
+//    public boolean isPalindrome(int x) {
+//        String s = String.valueOf(x);
+//        if (s.charAt(0) == '-') {
+//            return false;
+//        }
+//        for (int i = 0; i < s.length() / 2; i++) {
+//            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+}
