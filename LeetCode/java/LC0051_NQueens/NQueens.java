e81e84107f70b14818f4bdc2cf022f23288b9dff
akira37 <863781751@qq.com>
2025-01-01 23:25:04 +0800

    Revised from old uploads

---
+++ LeetCode/java/LC0051_NQueens/NQueens.java
@@ -0,0 +1,77 @@
+package LC0051_NQueens;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.com/problems/n-queens/">N-Queens</a>
+ * 数组；回溯
+ */
+public class NQueens {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solveNQueens(4));
+    }
+}
+
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (char[] row : board) {
+            Arrays.fill(row, '.');
+        }
+
+        dfs(res, board, n, 0);
+        return res;
+    }
+
+    private void dfs(List<List<String>> res, char[][] board, int n, int row) {
+        if (row == n) {
+            res.add(construct(board));
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isValid(board, n, row, col)) {
+                board[row][col] = 'Q';
+                dfs(res, board, n, row + 1);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private List<String> construct(char[][] board) {
+        List<String> res = new ArrayList<>();
+        for (char[] row : board) {
+            res.add(String.valueOf(row));
+        }
+        return res;
+    }
+
+    private boolean isValid(char[][] board, int n, int row, int col) {
+        // Check current column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check upper left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check upper right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ LeetCode/java/LC1160_FindWordsThatCanBeFormedByCharacters/FindWordsThatCanBeFormedByCharacters.java
@@ -0,0 +1,44 @@
+package LC1160_FindWordsThatCanBeFormedByCharacters;
+
+/**
+ * <a href="https://leetcode.cn/problems/find-words-that-can-be-formed-by-characters/">Find Words That Can Be Formed by Characters</a>
+ * 数组；哈希表；字符串；计数
+ */
+public class FindWordsThatCanBeFormedByCharacters {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.countCharacters(new String[]{"cat", "bt", "hat", "tree"}, "atach"));
+    }
+}
+
+class Solution {
+    public int countCharacters(String[] words, String chars) {
+        int[] charCounter = getCharCounter(chars);
+
+        int res = 0;
+        for (String word : words) {
+            int[] wordCount = getCharCounter(word);
+
+            boolean canSpell = true;
+            for (int i = 0; i < 26; i++) {
+                if (charCounter[i] < wordCount[i]) {
+                    canSpell = false;
+                    break;
+                }
+            }
+            if (canSpell) {
+                res += word.length();
+            }
+        }
+        return res;
+    }
+
+    private int[] getCharCounter(String chars) {
+        int[] charCounter = new int[26];
+        for (int i = 0; i < chars.length(); i++) {
+            char ch = chars.charAt(i);
+            charCounter[ch - 'a']++;
+        }
+        return charCounter;
+    }
+}
