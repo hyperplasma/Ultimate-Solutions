d5a41a191a5c478cdcfcca50c5a51e20acb8d805
akira37 <863781751@qq.com>
2025-01-03 20:34:30 +0800

    Add MarsCode problems

---
+++ Doubao MarsCode/java/MC1/Main.java
@@ -0,0 +1,32 @@
+package MC1;
+
+
+/**
+ * 1. 找单独的数
+ * 哈希表
+ */
+public class Main {
+    public static int solution(int[] cards) {
+        final int n = 1010;
+        int[] cnt = new int[n];
+
+        for (int card : cards) {
+            cnt[card]++;
+        }
+
+        int res = -1;
+        for (int card : cards) {
+            if (cnt[card] == 1) {
+                res = card;
+                break;
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        // Add your test cases here
+        System.out.println(solution(new int[]{7, 3, 3, 7, 10}));
+    }
+}
---
+++ Doubao MarsCode/java/MC30/Main.java
@@ -0,0 +1,33 @@
+package MC30;
+
+/**
+ * 30. 组成字符串ku的最大次数
+ * 哈希表
+ */
+public class Main {
+    public static int solution(String s) {
+        // 将字符串转换为小写以便统一处理
+        s = s.toLowerCase();
+
+        // 统计字符 'k' 和 'u' 的数量
+        int countK = 0;
+        int countU = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == 'k') {
+                countK++;
+            } else if (c == 'u') {
+                countU++;
+            }
+        }
+
+        // 返回 'k' 和 'u' 数量的最小值
+        return Math.min(countK, countU);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("AUBTMKAxfuu") == 1); // true
+        System.out.println(solution("KKuuUuUuKKKKkkkkKK") == 6); // true
+        System.out.println(solution("abcdefgh") == 0); // true
+    }
+}
