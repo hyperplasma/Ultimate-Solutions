b05cd63d6d64d11e072bea6c6c875f90865522ed
akira37 <863781751@qq.com>
2025-01-03 22:15:48 +0800

    Add Golang solutions

---
+++ LeetCode/go/LC0001_TwoSum/TwoSum.go
@@ -0,0 +1,13 @@
+package LC0001_TwoSum
+
+func twoSum(nums []int, target int) []int {
+	// 二分
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
