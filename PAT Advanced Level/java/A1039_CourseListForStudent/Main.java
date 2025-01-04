558a28f4f4d74efd4297d630260d28939d1c1921
akira37 <863781751@qq.com>
2025-01-04 20:18:09 +0800

    Add Java solutions

---
+++ PAT Advanced Level/java/A1039_CourseListForStudent/Main.java
@@ -0,0 +1,41 @@
+package A1039_CourseListForStudent;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+
+        HashMap<String, ArrayList<Integer>> hashMap = new HashMap<>();
+        for (int i = 0; i < k; i++) {
+            int courseID = scanner.nextInt();
+            int stuNum = scanner.nextInt();
+            for (int j = 0; j < stuNum; j++) {
+                String name = scanner.next();
+                if (!hashMap.containsKey(name)) {
+                    hashMap.put(name, new ArrayList<>());
+                }
+                hashMap.get(name).add(courseID);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            String name = scanner.next();
+            StringBuilder sb = new StringBuilder();
+            sb.append(name);
+            if (hashMap.containsKey(name)) {
+                ArrayList<Integer> arrayList = hashMap.get(name);
+                Collections.sort(arrayList);
+                sb.append(" ").append(arrayList.size());
+                for (Integer integer : arrayList) {
+                    sb.append(" ").append(integer);
+                }
+                System.out.println(sb);
+            } else {
+                System.out.println(name + " 0");
+            }
+        }
+    }
+}
