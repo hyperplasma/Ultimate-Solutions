450de0ede1d931a22e463489c904a2030178c6ed
Akira37 <863781751@qq.com>
2025-01-10 12:21:09 +0800

    3298. 统计重新排列后包含另一个字符串的子字符串数目II

---
+++ LeetCode/go/LC3298_CountSubstringsThatCanBeRearrangedToContainAStringII/count_substrings_that_can_be_rearranged_to_contain_a_string_ii_test.go
@@ -0,0 +1,10 @@
+package count_substrings_that_can_be_rearranged_to_contain_a_string_ii
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_validSubstringCount(t *testing.T) {
+	fmt.Println(validSubstringCount("bcca", "abc"))
+}
