b5cca53287ca44ad664c4708b58d9399a1a6ab93
Akira37 <863781751@qq.com>
2025-01-23 19:58:21 +0800

    Update heap

--- Algorithm_Templates/go/data_structure/heap/heap.go
+++ Algorithm_Templates/go/data_structure/heap/heap.go
@@ -2,16 +2,16 @@ package heap
 
 type MinHeap []int
 
-func (h MinHeap) Len() int {
-	return len(h)
+func (h *MinHeap) Len() int {
+	return len(*h)
 }
 
-func (h MinHeap) Less(i, j int) bool {
-	return h[i] < h[j]
+func (h *MinHeap) Less(i, j int) bool {
+	return (*h)[i] < (*h)[j]
 }
 
-func (h MinHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
+func (h *MinHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
 }
 
 func (h *MinHeap) Push(x interface{}) {
